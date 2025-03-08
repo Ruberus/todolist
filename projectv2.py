f9e0c852751920b39f80df06ef4997e8c2099be0
Ruberus <theunseriouspupu@gmail.com>
2025-03-08 17:30:36 +0530

    it kind of runs without updatnig the serial number
    Co-authored-by: Asmita Mitra <reokiiii@users.noreply.github.com>

--- projectv2.py
+++ projectv2.py
@@ -25,23 +25,60 @@ TaskName VARCHAR(1000) NOT NULL,
 Date DATE);'''
 cursor.execute(creation)
 
-def addtask(cursor,conn):
-    newtask=input('Enter your task:')
-    date= input('Enter the date (YYYY-MM-DD):')
-    taskq='''INSERT INTO todolist (Status,TaskName,Date) VALUES
-(%s,%s,%s)'''
-    values=('',newtask,date)
-    cursor.execute(taskq,values)
-    conn.commit()
+while True:
+    opt=int(input('What do you want to do?\n1. Add tasks\n2. View tasks\n3. Remove a task\n4. Mark task as done: '))
+    
+    def addtask(cursor,conn):
+        newtask=input('Enter your task:')
+        date= input('Enter the date (YYYY-MM-DD):')
+        taskq='''INSERT INTO todolist (Status,TaskName,Date) VALUES
+    (%s,%s,%s)'''
+        values=('',newtask,date)
+        cursor.execute(taskq,values)
+        conn.commit()
 
+    def viewtasks(cursor,conn):
+        cursor.execute("SELECT * FROM todolist;")
+        data=cursor.fetchall()
+        print(data)
 
-addtask(cursor,conn)
+    def deltask(cursor,conn):
+        que=int(input('Enter the serial number of task you want to delete: '))
+        taskd=''' DELETE FROM todolist 
+        WHERE SerialNumber=%s;'''
+        cursor.execute(taskd,(que,))
+        conn.commit()
 
-cursor.execute("SELECT * FROM todolist;")
-data=cursor.fetchall()
-print(data)
+    def marktask(cursor,conn):
+        taskw=int(input('Enter the Serial Number of the task:'))
+        taskq='''UPDATE todolist
+        SET Status='✓'
+        WHERE SerialNumber=%s;'''
+        cursor.execute(taskq,(taskw,))
+        conn.commit()
+
     
 
+    
+    
+    if opt!=1 and opt!=2 and opt!=3 and opt!=4:
+        print("please choose a valid option")
+    elif opt==1:
+        viewtasks(cursor,conn)
+        addtask(cursor,conn)
+    elif opt==2:
+        viewtasks(cursor,conn)
+    elif opt==3:
+        viewtasks(cursor,conn)
+        deltask(cursor,conn)
+    elif opt==4:
+        marktask(cursor,conn)
+        viewtasks(cursor,conn)
+       
+
+    opt3=input('Do you want to end the program? Y/N:')
+    if opt3=='Y' or opt3=='y':
+        break
 
 
 
